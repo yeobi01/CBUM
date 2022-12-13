f7236aed3285c94368048abe64706cfa4701bff1
yeobi01 <yeobi_01@naver.com>
2022-12-14 05:10:00 +0900

    add css

--- js/background.js
+++ js/background.js
@@ -5,6 +5,8 @@ const images = [
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
-document.body.appendChild(bgImage);
+//const bgImage = document.createElement("img");
+//bgImage.src = `img/${chosenImage}`;
+
+document.body.style.backgroundImage = `url(img/${chosenImage})`;
+
--- js/todo.js
+++ js/todo.js
@@ -23,10 +23,10 @@ function paintToDo(newTodo){
     const span = document.createElement("span");
     span.innerText = newTodo.text;
     const button = document.createElement("button");
-    button.innerText = "X";
+    button.innerText = "reset";
     button.addEventListener("click", deleteToDo);
-    li.appendChild(span);
     li.appendChild(button);
+    li.appendChild(span);
     toDoList.appendChild(li);
 }
 
