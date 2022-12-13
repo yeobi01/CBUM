1e8ddf22fe555f080e5b818a8255de9ed06f7793
yeobi01 <yeobi_01@naver.com>
2022-12-14 02:21:30 +0900

    add weather api

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "0be2f24b7f28e953baabe8f688d953aa";
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError(){
+    alert("Can't find you. No weather for you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
