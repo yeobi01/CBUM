9f31c9b5072cbc6b4e62a5131c0ead5e18c2f143
yeobi01 <yeobi_01@naver.com>
2022-12-14 12:42:58 +0900

    add weather api

--- js/weather.js
+++ js/weather.js
@@ -3,14 +3,15 @@ const API_KEY = "0be2f24b7f28e953baabe8f688d953aa";
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
+        const weather = document.querySelector("#weather span:last-child");
+        const city = document.querySelector("#weather span:first-child");
         city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        weather.innerText = `${data.weather[0].main} ${data.main.temp}℃`;
     });
 }
 function onGeoError(){
